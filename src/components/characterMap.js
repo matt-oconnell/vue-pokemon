7bb278cb260a1edaac17e56bd27f5859ca7962c6
matt-oconnell <mattoconnell408@gmail.com>
2017-06-29 15:43:03 -0400

    fonts

---
+++ src/components/characterMap.js
@@ -0,0 +1,13 @@
+export default {
+  xyz: {
+    name: 'xyz',
+    hp: 57,
+    level: 10,
+    attacks: {
+      scratch: 35,
+      growl: 40,
+      ember: 25,
+      smokescreen: 20,
+    }
+  }
+}
